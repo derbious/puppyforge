878d78c34ebcf9810efbdd48ed892dc69dfaa259
steven.anthony <steven.anthony@noaa.gov>
2014-09-22 13:20:51 -0400

    adding command line arg parsing

--- main.go
+++ main.go
@@ -3,36 +3,55 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
+	//"os"
 	"path/filepath"
+  "flag"
 )
 
-func main() {
-	port := os.Getenv("PORT")
-	modulePath := os.Getenv("MODULEPATH")
 
+// Initialize the application. Read the config options in the following hierarchy
+//   command line args         <= highest priority
+//   supplied config file
+//   /etc/puppyforge.conf  <= lowest priority
+func ReadConfig() (string, string) {
+  // Read the flags, determine if 
+  port := flag.String("port", "80", "The port number to listen on.")
+  modpath := flag.String("modulepath", "", "The path to search for the modules.")
+  configfile := flag.String("config", "/etc/puppyforge.conf", "The location of the config file.")
+  flag.Parse()
+  log.Println(*port, *modpath, *configfile)
+  if configfile != "/etc/puppyforge.conf" {
+  }
+  return *port, *modpath
+}
+
+
+func main() {
+  port, modulepath := ReadConfig()
+
+  log.Fatal("testing")
 	if len(port) == 0 {
 		log.Fatal("Missing PORT environment variable")
 	}
-	if len(modulePath) == 0 {
+	if len(modulepath) == 0 {
 		log.Fatal("Missing MODULEPATH environment variable")
 	}
 
-	log.Println("Starting go-puppet-forge on port", port, "serving modules from", modulePath)
+	log.Println("Starting puppyforge on port", port, "serving modules from", modulepath)
 
   // declare the webservice endpoints
 	http.HandleFunc("/v3/modules", func(w http.ResponseWriter, r *http.Request) {
-		ModulesHandler(w, r, modulePath)
+		ModulesHandler(w, r, modulepath)
 	})
   http.HandleFunc("/v3/modules/", func(w http.ResponseWriter, r *http.Request) {
-    ModulesHandler(w, r, modulePath)
+    ModulesHandler(w, r, modulepath)
   })
 
 	http.HandleFunc("/v3/files/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, filepath.Join(modulePath, r.URL.Path[10:]))
+		http.ServeFile(w, r, filepath.Join(modulepath, r.URL.Path[10:]))
 	})
 	http.HandleFunc("/v3/releases", func(w http.ResponseWriter, r *http.Request) {
-		ReleasesHandler(w, r, modulePath)
+		ReleasesHandler(w, r, modulepath)
 	})
 
 	http.ListenAndServe(":"+port, nil)
